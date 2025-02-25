22158a7bea9ad532cc31b10cc76a01e69875bf6b
Tomasz Dębosz <98646427+Dosztosz@users.noreply.github.com>
2025-02-25 23:25:24 +0100

    up

--- mild_calc/src/server.js
+++ mild_calc/src/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
@@ -22,23 +21,17 @@ connectToDatabase();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-let submissionCount = 0;
-
 app.use(express.urlencoded({ extended: true }));
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async (req, res) => {
-    submissionCount = parseInt(req.query.submissionCount, 10) || 0;
     try {
         const database = client.db('test');
         const collection = database.collection('test');
         const lobs = await collection.distinct('LOB');
         const names = await collection.distinct('NAME');
-        res.render('index', { data: [], lobs, names, selectedLob: null, selectedName: null, submissionCount: submissionCount });
+        res.render('index', { data: [], lobs, names, selectedLob: null, selectedName: null });
     } catch (error) {
         console.error("Error fetching data:", error);
         res.status(500).send("Error fetching data");
@@ -46,34 +39,25 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/filter', async (req, res) => {
-    submissionCount = parseInt(req.body.submissionCount, 10) || 0;
-
-    if (submissionCount < 2) {
-        submissionCount++;
-        res.redirect('/?submissionCount=' + submissionCount);
-    } else {
-        // Reset submission count and handle the form data
-        submissionCount = 0;
-        try {
-            const selectedLob = req.body.lob || null;
-            const selectedName = req.body.name || null;
-            const database = client.db('test');
-            const collection = database.collection('test');
-            const query = {};
-            if (selectedLob) {
-                query.LOB = selectedLob;
-            }
-            if (selectedName) {
-                query.NAME = selectedName;
-            }
-            const data = await collection.find(query).toArray();
-            const lobs = await collection.distinct('LOB');
-            const names = selectedLob ? await collection.distinct('NAME', { LOB: selectedLob }) : [];
-            res.render('index', { data, lobs, names, selectedLob, selectedName, submissionCount: submissionCount });
-        } catch (error) {
-            console.error("Error fetching data:", error);
-            res.status(500).send("Error fetching data");
+    try {
+        const selectedLob = req.body.lob || null;
+        const selectedName = req.body.name || null;
+        const database = client.db('test');
+        const collection = database.collection('test');
+        const query = {};
+        if (selectedLob) {
+            query.LOB = selectedLob;
         }
+        if (selectedName) {
+            query.NAME = selectedName;
+        }
+        const data = await collection.find(query).toArray();
+        const lobs = await collection.distinct('LOB');
+        const names = selectedLob ? await collection.distinct('NAME', { LOB: selectedLob }) : [];
+        res.render('index', { data, lobs, names, selectedLob, selectedName });
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        res.status(500).send("Error fetching data");
     }
 });
 
