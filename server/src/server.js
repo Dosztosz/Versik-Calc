b47c5706c46bf98e88feb3f3e5490a67e1c08001
Tomasz Dębosz <98646427+Dosztosz@users.noreply.github.com>
2025-02-25 23:40:43 +0100

    u

--- server/src/server.js
+++ server/src/server.js
@@ -26,7 +26,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.urlencoded({ extended: true }));
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async (req, res) => {
